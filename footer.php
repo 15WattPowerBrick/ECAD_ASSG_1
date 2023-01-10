1aefe122ea99837e8b3d2913f5ce8b0177e8c6f4
TeoWeiShan <s10205156@connect.np.edu.sg>
2023-01-11 00:50:47 +0800

    Added pages

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div> <!-- close the column in 3rd Row -->
+</div> <!-- close the 3rd Row -->
+<!-- 4th Row -->
+<div class="row" >
+ <div class="col-sm-12" style="text-align: right; ">
+     <hr />
+    Drop an email to:
+     <a href="mailto:mamaya@np.edu.sg"> help@happybaby.com</a>
+     <p style="font-size:12px">&copy;Copyright by Happy Baby</p>
+</div>
+</div>
+</div> 
+<!-- Close the bootstrap container -->
+</body>
+</html>
